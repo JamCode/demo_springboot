0e540e4a8b4804547ecc2b503f959da39738e976
wanghan <wh85125@163.com>
2019-01-23 11:40:04 +0800

    #

--- src/main/java/com/wanghan/demo/api/HelloController.java
+++ src/main/java/com/wanghan/demo/api/HelloController.java
@@ -2,6 +2,8 @@ package com.wanghan.demo.api;
 
 import com.wanghan.demo.dao.UserMapper;
 import com.wanghan.demo.models.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,10 +15,18 @@ public class HelloController {
     @Autowired
     private UserMapper userMapper;
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String index() {
 
         User u = userMapper.findByName("iDeal_test005");
+
+
+        logger.info("test traceid");
+
+
 
         System.out.println(u.getUserid());
         System.out.println(u.getPhone_num());
